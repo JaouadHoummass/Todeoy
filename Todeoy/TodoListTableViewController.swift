33cfb1a76db9cd04b69618b638e67549f23729f1
Jaouad Hoummass <jaouadhoummass@jaouads-mac.local>
2019-02-13 13:05:51 +0100

    add item fonctionnalité

--- Todeoy/TodoListTableViewController.swift
+++ Todeoy/TodoListTableViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class TodoListTableViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos","Destroy Demogorgon" ]
+    
+    var itemArray = ["Find Mike", "Buy Eggos","Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +35,7 @@ class TodoListTableViewController: UITableViewController {
 
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
 
         // Configure the cell...
@@ -44,66 +46,51 @@ class TodoListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        //print(itemArray[indexPath.row])
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
           
-        
         tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    
             
         } else {
             
            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
+          
         }
-        
         
        tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    //MARK: Add Item to todoey List
+   
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        
+        //creer un pointeur qui va pointer sur le alertTextField pour recuperer la valeur du text
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (alertTextfield) in
+            
+          alertTextfield.placeholder = "Please Add New Item"
+          textField = alertTextfield
+            
+        }
+        
+        
+        
+       present(alert, animated: true, completion: nil)
+    
+}
+    
 }
